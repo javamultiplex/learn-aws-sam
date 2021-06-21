07fcf93d51ef4131354bf76a3ab6f332116b6c07
Rohit Agarwal <programmingwithrohit@gmail.com>
2021-06-21 18:55:42 +0530

    Added cognito and http-api templates

---
+++ cognito/src/app.js
@@ -0,0 +1,15 @@
+exports.lambdaHandler = async (event, context, callback) => {
+	let newScopes = event.request.groupConfiguration.groupsToOverride.map(
+		item => `${item}-${event.callerContext.clientId}`
+	);
+
+	event.response = {
+		claimsOverrideDetails: {
+			claimsToAddOrOverride: {
+				scope: newScopes.join(' '),
+			},
+		},
+	};
+
+	callback(null, event);
+};
